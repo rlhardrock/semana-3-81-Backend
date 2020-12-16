7461a573a10973b5bbcba253dfcee96df1e8926f
Louis Rex <rlhardrock@gmail.com>
2020-12-15 21:27:48 -0500

    falla de enlace

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -42,47 +42,6 @@ exports.singin = async(req, res, next) =>{
     }
 };
 
-exports.login = async(req, res, next) =>{
-    try{
-        const user = await models.user.findOne({where: {email:req.body.email}});
-        if (user) {
-            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-            if (passwordIsValid) {
-                /* const token = await  */
-                const token = jwt.sign({
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    rol: user.rol,
-                    status: user.status
-                },'config.secret',{
-                    expiresIn: 86400,
-                }
-                );
-                res.status(200).send({
-                    auth: true,
-                    accessToken: token,
-                    // user: user
-                })
-            }else{
-                res.status(401).json({
-                    error: 'Error 401'
-                })
-            }
-        }else{
-            res.status(404).json({
-                error: 'Error 404'
-            })
-        }
-    }catch(error){
-        res.status(500).send({
-        message:'Error-500'
-        }),
-    next(error);
-    }
-};
-
-
 exports.register = async(req, res, next) => {
     try{
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,10 @@ const models = require('../../models');
 const UserController = require('../../controllers/UserController.js');
 const bcrypt = require('bcryptjs');
 
+
+
+
+
 router.get('/',async(req, res) => {
     const user = await models.user.findAll();
     res.status(200).json(user);
@@ -13,9 +17,6 @@ router.post('/register',async(req, res) => {
     const user = await models.user.create(req.body);
     res.status(200).json(user);
 });
-
-/* router.post('/login', usuarioController.login); */
-router.post('/login', UserController.login);
 
 
 router.post('/singin', UserController.singin);
